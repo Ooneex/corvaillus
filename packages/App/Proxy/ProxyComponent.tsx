be7b4ca2a7b81c9bc3f847916585add013302c58
fkdiomande <fkdiomande.dev@gmail.com>
2023-01-01 17:16:06 +0200

    feat(app/config): parse app default config

--- packages/App/Proxy/ProxyComponent.tsx
+++ packages/App/Proxy/ProxyComponent.tsx
@@ -1,6 +1,8 @@
 import {PageProps} from "../deps.ts";
 
+// Todo: Refactor to ProxyView
 export const ProxyComponent = (Props: PageProps) => {
   // console.log(Props.data.app.env.getPort());
+  console.log(Props.data.app);
   return <h1>Proxy Component</h1>;
 };
--- packages/App/Proxy/ProxyMiddleware.ts
+++ packages/App/Proxy/ProxyMiddleware.ts
@@ -1,10 +1,17 @@
 import {DotEnvValueType, MiddlewareHandlerContext} from "../deps.ts";
 import { env } from "../Env/Env.ts";
 import {IEnv} from "../Env/IEnv.ts";
+import {appConfig} from "../Config/AppConfig.ts";
+import {AppDirectoryType} from "../Directory/types.ts";
+import {AppConfigErrorType} from "../Config/types.ts";
 
 interface IMiddlewareState {
   app: {
     env: IEnv;
+    config: {
+      directories: AppDirectoryType | null;
+      errors: AppConfigErrorType | null;
+    };
   }
 }
 
@@ -13,9 +20,23 @@ export class ProxyMiddleware {
     request: Request,
     ctx: MiddlewareHandlerContext<IMiddlewareState>,
   ) {
+
+    // Env variables
     const envData: Record<string, DotEnvValueType> = JSON.parse(Deno.env.get("OONEEX_APP_ENV") as string);
     env.setData(envData);
-    ctx.state.app = {env};
+
+    // App config
+    // const appConfigData: AppConfigType = JSON.parse(localStorage.getItem("OONEEX_APP_APP_CONFIG") as string);
+    // const directory = new AppDirectory(appConfigData.directories);
+
+    ctx.state.app = {
+      env,
+      config: {
+        directories: appConfig.getDirectories(),
+        errors: appConfig.getErrors()
+      }
+    };
+
     return await ctx.next();
   }
 }
